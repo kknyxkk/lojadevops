b353fb3fc4dbb1fdd33c735a93709c182d90793d
Carlos Santana <chenrique@indracompany.com>
2017-09-14 14:33:13 -0300

    Alterando layout pagina de login

--- Lojadevops/src/main/java/br/com/indra/jovemprofissional/Configuracao.java
+++ Lojadevops/src/main/java/br/com/indra/jovemprofissional/Configuracao.java
@@ -13,13 +13,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @SpringBootApplication
 
 public class Configuracao {
-	
-    public static void main(String[] args){
-    	
-        SpringApplication.run(Configuracao.class, args);
-    }
-    
-   /* @Bean
+
+	public static void main(String[] args){
+
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+	/* @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
@@ -28,8 +28,8 @@ public class Configuracao {
         dataSource.setPassword("loja");
         return dataSource;
     }*/
-    
-    
-    
-   
+
+
+
+
 }
--- Lojadevops/src/main/java/br/com/indra/jovemprofissional/LojaController.java
+++ Lojadevops/src/main/java/br/com/indra/jovemprofissional/LojaController.java
@@ -8,51 +8,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LojaController {
-//Mapeamento do Index
+	//Mapeamento do Index
 	@RequestMapping("/")
 	public String index(){
 		return "indexLoja";
 	}
-	
+	//mapeamento da pagina de produtos
 	@RequestMapping("/produtos")
 	public String produtos(){
 		return "produtos";
 	}
-	
-	 @RequestMapping(value= "logar", method = RequestMethod.POST)
-	    public String verificar(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
-	    {
-	    	String resul="teste";
-	    	
-	    	if(!valida(username) && !valida(password)){
-	    		System.out.println("campo em branco");
-	    		return "indexLoja";
-	    	}	    	
-	    	
-	        if(username.equals(resul) && password.equals(resul))
-	        {	        
-	        	System.out.println("logou");
-	        	//Proxima pagina-->
-	        	return "produtos";
-	        	
-	    	}else{	    		
-	    		System.out.println("erro login");
-	    		return "indexLoja";
-	    	}
-	    		
-	    }
-	 
-	 //metodo de validação para inputs diferentes de null
-	 public Boolean valida(String entrada)
-	    {
-	    	if(!entrada.isEmpty())
-	    	{
-	    		return true;
-	    	}else {
-	    		return false;
-			}
-	    }
-	 
+
+	@RequestMapping(value= "logar", method = RequestMethod.POST)
+	public String verificar(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
+	{
+		String resul="teste";
+
+		if(!valida(username) && !valida(password)){
+			System.out.println("campo em branco");
+			return "indexLoja";
+		}
+
+		if(username.equals(resul) && password.equals(resul))
+		{	        
+			System.out.println("logou");
+			//Proxima pagina-->
+			return "produtos";
+
+		}else{	    		
+			System.out.println("erro login");
+			return "indexLoja";
+		}
+
+	}
+
+	//metodo de validação para inputs diferentes de null
+	public Boolean valida(String entrada)
+	{
+		if(!entrada.isEmpty())
+		{
+			return true;
+		}else {
+			return false;
+		}
+	}
+
 	//Mapeamento do ListaClientes
 	/*@RequestMapping("listaconvidados")
 	public String listaConvidados(Model model){
@@ -62,18 +62,18 @@ public class LojaController {
 
 		return "listaconvidados";
 	}*/
-	
+
 	//Mapeamento do listaequipamentos
 	/*@RequestMapping("listaequipamentos")
 	public String listaEquipamentos(Model model){
-		
+
 		Iterable<Equipamento> equips = repositoryEquip.findAll();
 		model.addAttribute("listaEquipamentos", equips);
-		
-		
+
+
 		return "listaequipamentos";
 	}*/
-	
+
 	//Mapeamento para o metodo salvar
 	/*@RequestMapping(value= "salvar", method = RequestMethod.POST)
 	public String salvar(@RequestParam("ID_CLIENTE") int ID_CLIENTE, @RequestParam("NO_CLIENTE") String NO_CLIENTE, Model model){
@@ -89,22 +89,22 @@ public class LojaController {
 	//Mapeamento para o metodo deletar
 	/*@RequestMapping(value= "deletar", method = RequestMethod.POST)
 	public String deletar(@RequestParam("ID_CLIENTE") int ID_CLIENTE, Model model){
-		
+
 		Cliente delCliente = repositoryCli.findOne(ID_CLIENTE);
 		repositoryCli.delete(delCliente);	
-		
+
 		Iterable<Cliente> clientes = repositoryCli.findAll();
 		model.addAttribute("listaClientes", clientes);
 
 		return "listaconvidados";
 	}*/
-	
+
 	/*@Autowired											//
 	private ClienteRepository repositoryCli;				//
 															//Amarração com objeto do BD
 	@Autowired												//
 	private EquipamentoRepository repositoryEquip;			//
-	*/
+	 */
 
 
 
